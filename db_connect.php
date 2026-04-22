b231964e23d3fa97eb3512acf1ea99f0d5d952b5
mguz1 <45877621+mguz1@users.noreply.github.com>
2026-04-22 14:33:13 -0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,7 @@ $user = "zID"; // MariaDB
 $pass = "Password"; // MariaDB
 
 try{
-	$pdo = new PDO("mysql:host=$host;dbname=$db", $user, #pass);
+	$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e){
 	die("Connection failed: " . $e->getMessage());
